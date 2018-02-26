3b13b68a7fd61bae558eca869bad68ed553c40a8
Richard Ngo <rngobile@gmail.com>
2018-02-25 23:14:05 -0500

    created NewGeneratePassword, stripped python modules

--- NewGeneratePassword.py
+++ NewGeneratePassword.py
@@ -4,7 +4,21 @@ import random
 class NewGeneratePassword:
 
     def __init__(self):
-	self.char_set = 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ^!%/=?{[]}+~#-_.:,;<>|\\'
+        self.char_set = 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ^!%/=?{[]}+~#-_.:,;<>|\\'
+        self.passwordList = []
+        self.oldChar = ''
+        self.newChar = ''
+        self.password = ''
 
     def generate_pass(self, length=25):
-	return "".join(random.sample(self.char_set,length))
+        #return "".join(random.sample(self.char_set,length))
+        for i in range(length):
+            self.newChar =  self.char_set[random.randrange(len(self.char_set))]
+            if (i == 0) or (self.newChar != self.oldChar):
+                self.passwordList.append(self.newChar)
+                self.oldChar = self.newChar
+            else:
+                i-=1
+        
+        self.password = ''.join(self.passwordList)
+        return self.password
