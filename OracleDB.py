cae708f062b3975390862272d807fbe2c871d17a
Richard Ngo <rngobile@gmail.com>
2018-02-27 17:27:35 -0500

    readded OracleDB with zxJDBC module

---
+++ OracleDB.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+from com.ziclix.python.sql import zxJDBC
+
+class OracleDB:
+    def __init__(self, dsURL, user, password, dsDriver):
+        self.dsURL = dsURL
+        self.user = user
+        self.password = password
+        self.dsDriver = dsDriver
+
+        self.connection = zxJDBC.connect(self.dsURL, self.user, self.password, self.dsDriver)
+    
+    def changePassword(self, newPassword):
+        self.newPassword = newPassword
+        self.cursor = self.connection.cursor()
+        try:
+            sql = 'alter user %s identified by %s' % (self.user, self.newPassword)
+            self.cursor.execute(sql)
+            #self.connection.commit()
+        finally:
+            self.connection.close()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.connection:
+            self.connection.close()
